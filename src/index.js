f091a8de599dd579193a0afd8325f3db17e9a7fe
brysonmckenzie <brysonmckenzie83@gmail.com>
2018-11-05 16:38:07 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
+import SearchBar from './components/SearchBar';
 import request from 'superagent';
 import './styles/app.css';
 
-
 class App extends React.Component {
-
-
-
     constructor() {
-        super()
+        super();
 
         this.state = {
             gifs: []
         };
-
-        this.handleTermChange = this.handleTermChange.bind(this);
     }
 
-
-
-    handleTermChange = (term) => {
-  
-
-
+    handleTermChange(term) {
         const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=fhruhErb7kOixSYUM2EV916C9qVt2wiL`;
 
         request.get(url, (err, res) => {
@@ -33,20 +22,14 @@ class App extends React.Component {
         });
     }
 
-
     render() {
-
-
         return (
-            <div className="container">
-                <h1 className="giphy" >GIPHY Search</h1>
-                <SearchBar onTermChange={term => this.handleTermChange(term)} />
-                <GifList gifs={this.state.gifs} />
+            <div>
+                <SearchBar onTermChange={term => this.handleTermChange(term)}/>
+                <GifList gifs={this.state.gifs}/>
             </div>
         );
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
